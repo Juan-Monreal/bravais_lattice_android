2d197f8682f53ceceffcdfdd1702c6c2493df75b
jmonrealr <jcmonrealr@gmail.com>
2022-08-11 08:57:19 -0500

    add: Button on opngl activyt

--- app/src/main/java/com/upv/pm_2022/iti_27849_u3_equipo_01/OpenGLActivity.java
+++ app/src/main/java/com/upv/pm_2022/iti_27849_u3_equipo_01/OpenGLActivity.java
@@ -2,23 +2,37 @@ package com.upv.pm_2022.iti_27849_u3_equipo_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class OpenGLActivity extends AppCompatActivity {
 
     private CustomSurfaceView customSurfaceView;
+    private LinearLayout primaryLayout;
+    private Button btnChange;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_open_glactivity);
+        setContentView(R.layout.activity_open_glactivity);
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
 
+        primaryLayout = findViewById(R.id.primaryLayout);
+        btnChange = new Button(this);
+        btnChange.setText(R.string.text_change);
+//        btnChange.setOnClickListener();
+        btnChange.setBackgroundColor(Color.parseColor("#00BCD4"));
+
         customSurfaceView = new CustomSurfaceView(this, size.x, size.y);
-        setContentView(customSurfaceView);
+
+        primaryLayout.addView(btnChange, 0);
+        primaryLayout.addView(customSurfaceView, 1);
     }
 
     @Override
