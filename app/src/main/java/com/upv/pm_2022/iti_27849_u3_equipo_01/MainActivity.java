4093e39eec55d0fb7345953ae3ed027803b41099
Crxwler <loco7139@gmail.com>
2022-08-10 23:04:26 -0500

    add: Toolbar

--- app/src/main/java/com/upv/pm_2022/iti_27849_u3_equipo_01/MainActivity.java
+++ app/src/main/java/com/upv/pm_2022/iti_27849_u3_equipo_01/MainActivity.java
@@ -1,14 +1,44 @@
 package com.upv.pm_2022.iti_27849_u3_equipo_01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.ui.AppBarConfiguration;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AppBarConfiguration appBarConfiguration;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_about) {
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
